c71c97c10762eeb4d5792df2b8d99c12f76d5d9d
cielo-illust <the_illustrations_of_cielo@hotmail.co.jp>
2016-01-10 02:30:57 +0900

    alpha

---
+++ Assets/Scripts/ClearTrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClearTrigger : MonoBehaviour {
+	GameObject gameController;
+	void Start () {
+		gameController = GameObject.FindWithTag ("GameController");
+	}
+	void OnTriggerExit2D () {
+		gameController.SendMessage ("IncreaseScore");
+	}
+}
